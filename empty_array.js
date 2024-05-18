b97bb1810945366d5e2306abc8fea07319f60288
gorgeous-lady1 <167902564+gorgeous-lady1@users.noreply.github.com>
2024-05-18 16:13:09 -0700

    Add files via upload

---
+++ empty_array.js
@@ -0,0 +1,9 @@
+// think of something you could store in a array
+let items = ['Pakistan', 'Karachi', 'Urdu'];
+// write a program that creates a list containing these items.
+let item2 = [];
+item2.push('Palestine');
+item2.push('Gaza');
+item2.push('Islam');
+console.log(item2);
+export {};
